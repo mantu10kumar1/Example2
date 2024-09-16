788183058e41ba1b85be4abf821f050dc731108d
Mantukumar <mantukumar4376@gmail.com>
2024-09-16 16:29:53 +0530

    Hello2 is only for testing

--- src/com/hello/Hello2.java
+++ src/com/hello/Hello2.java
@@ -1,5 +1,7 @@
 package com.hello;
 
 public class Hello2 {
-
+	public static void main(String[] args) {
+		System.out.println("This is the testing for pushing the projects on the github repository...");
+	}
 }
